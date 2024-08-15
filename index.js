4239a7ee6f94a3384b83091dcc8cd5bccd578fa7
Taiwo Emmanuel <taiwomichealemmanuel@gmail.com>
2024-08-15 10:02:36 +0100

    Deployed Calculator project

---
+++ index.js
@@ -0,0 +1,23 @@
+function showNumber(num) {
+    if (document.getElementById('displayNumber').innerText == 0){
+        document.getElementById('displayNumber').innerText =num;
+    }else{
+        document.getElementById('displayNumber').innerText +=num
+    }
+}
+
+function calculate() {
+    let userCalculation = document.getElementById('displayNumber').innerText;
+    let calculation =eval(userCalculation);
+    document.getElementById('displayNumber').innerText = calculation
+}
+function clearButton() {
+    document.getElementById('displayNumber').innerText = "0"
+}
+function backSpace() {
+    var display = document.getElementById('displayNumber').innerText;
+    if(display.length > 1) {
+        display = display.slice(0, -1);
+        document.getElementById('displayNumber')
+    }
+}
